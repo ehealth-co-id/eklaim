e823d6b71bdd239295c84500091152673d9db196
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2025-07-02 15:29:56 +0700

    v5.9.2

--- start.ps1
+++ start.ps1
@@ -1,7 +1,33 @@
 Set-Culture id-ID
 Set-WinSystemLocale id-ID
 Set-WinUserLanguageList id-ID -Force
-Copy-Item -Recurse C:\E-Klaim\restore C:\E-Klaim\backup\restore
+
+$OldErrorActionPreference = $ErrorActionPreference
+try {
+    $ErrorActionPreference = 'SilentlyContinue'
+    $version = Get-Content -Path "C:\E-Klaim\version.txt" -Raw
+} catch {
+    $version = ""
+} finally {
+    $ErrorActionPreference = $OldErrorActionPreference
+}
+
+if (! $version -eq "5.9.2") {
+    Write-Host "Patching..."
+    ((Get-FileHash patch.exe).Hash -eq '4E599F199CA8C7A2118424FA4A348A204C7FA0C4DBA08D63FDBDC99797A7E127') -or $(exit 1)
+    Start-Process -FilePath 'patch.exe' -ArgumentList @('/VERYSILENT', '/SP-', '/NORESTART', '/SUPPRESSMSGBOXES') -Wait
+    Set-Content c:\E-Klaim\version.txt '5.9.2' -NoNewline;
+    Write-Host "Patching done."
+}
+
+Write-Host "Configuring..."
+(Get-Content C:\E-Klaim\server.ini).Replace('debuglog = 0', 'debuglog = 1') | Set-Content C:\E-Klaim\server.ini
+(Get-Content C:\E-Klaim\server.ini).Replace('errorlog = 0', 'errorlog = 1') | Set-Content C:\E-Klaim\server.ini
+Set-Content -Path "C:\E-Klaim\tmp\php-error.log" -Value $null
+Set-Content -Path "C:\E-Klaim\tmp\debuglog" -Value $null
+Write-Host "Configuring done."
+
+Write-Host "Starting services..."
 $env:PATH += ";C:\E-Klaim\include\7za465"
 $eklaim_php_error_log = Start-Process -NoNewWindow powershell { Get-Content -Tail 0 -Wait C:\E-Klaim\tmp\php-error.log | C:\decode.ps1 } -PassThru
 $eklaim_php_debug_log = Start-Process -NoNewWindow powershell { Get-Content -Tail 0 -Wait C:\E-Klaim\tmp\debuglog | C:\decode.ps1 } -PassThru
@@ -10,4 +36,5 @@ $mysql = Start-Process -NoNewWindow -WorkingDirectory "C:\xampp" powershell { .\
 $apache_access_log = Start-Process -NoNewWindow -WorkingDirectory "C:\xampp" powershell { Get-Content -Tail 0 -Wait .\apache\logs\access.log } -PassThru
 $apache_error_log = Start-Process -NoNewWindow -WorkingDirectory "C:\xampp" powershell { Get-Content -Tail 0 -Wait .\apache\logs\error.log } -PassThru
 $apache_php_error_log = Start-Process -NoNewWindow -WorkingDirectory "C:\xampp" powershell { Get-Content -Tail 0 -Wait .\apache\logs\php_error.log } -PassThru
+Write-Host "Services started."
 $apache,$mysql | wait-process
